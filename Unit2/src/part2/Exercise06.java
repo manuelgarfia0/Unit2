964460aff4127e4005b2b2e4375f2a5fd8c23932
manuel.garfia <manuel.garfia@106-10.info2>
2025-10-17 12:07:25 +0200

    Boletin 2 terminado

--- Unit2/src/part2/Exercise06.java
+++ Unit2/src/part2/Exercise06.java
@@ -8,7 +8,26 @@ public class Exercise06 {
 		// Create scanner
 		Scanner sc = new Scanner(System.in);
 		// Declare variables
-		
+		int num1;
+		int num2;
+		int num3;
+		// Ask data to user
+		System.out.println("Introduce 3 numbers: ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+		// Check if the addition of two of the numbers, the answer is the other number
+		if (num1 + num2 == num3) {
+			System.out.println("The addition of " + num1 + " + " + num2 + " = " + num3);
+		} else if (num1 + num3 == num2) {
+			System.out.println("The addition of " + num1 + " + " + num3 + " = " + num2);
+		} else if (num2 + num3 == num1) {
+			System.out.println("The addition of " + num2 + " + " + num3 + " = " + num1);
+		} else {
+			System.out.println("The addition of two numbers, the answer isn't the other number.");
+		}
+		// Close scanner
+		sc.close();
 	}
 
 }
