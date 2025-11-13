f444461e10d1e4ada08e26073904e06da9e53998
manuel.garfia <manuel.garfia@106-10.info2>
2025-11-13 13:15:10 +0100

    Ejercicio 8 mejorado

--- Unit2/src/part6/Exercise08.java
+++ Unit2/src/part6/Exercise08.java
@@ -19,14 +19,9 @@ public class Exercise08 {
 		numB = sc.nextInt();
 		// See the numbers between A and B
 		// If numB is higher than numA show numbers from numA to numB
-		if (numA < numB) {
-			lower = numA;
-			higher = numB;
-			// If numA is higher than numB show numbers from numB to numA
-		} else {
-			higher = numA;
-			lower = numB;
-		}
+		lower = Math.min(numA, numB);
+		// If numA is higher than numB show numbers from numB to numA
+		higher = Math.max(numA, numB);
 		// Show numbers from lower number to higher
 		for (int i = lower; i <= higher; i++) {
 			System.out.println(i);
