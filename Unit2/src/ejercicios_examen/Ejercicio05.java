a59e5da1ef209edef0a456b2db9018557bd1fbfe
manuelgarfia0 <manuel.garfia@iesnervion.es>
2025-11-27 16:42:53 +0100

    Bucles terminado

--- Unit2/src/ejercicios_examen/Ejercicio05.java
+++ Unit2/src/ejercicios_examen/Ejercicio05.java
@@ -9,19 +9,29 @@ public class Ejercicio05 {
 		Scanner sc = new Scanner(System.in);
 		// Declarar variables
 		int num;
+		boolean blanco;
 		// Preguntar al usuario tamaño del escaque
 		System.out.println("Introduce el tamaño del escaque: ");
 		num = sc.nextInt();
 		// Bucle para las filas del tablero
-		for (int i = 0; i < 4; i++) {
-			// Bucle para los escaques
-			for (int j = 0; j < num; j++) {
-				for (int k = 0; k < num; k++) {
-					System.out.print("#");
+		for (int i = 0; i < 8; i++) {
+			// Bucle para las filas de los escaques
+			for (int k = 0; k < num; k++) {
+				// Bucle para las columnas del tablero
+				for (int j = 0; j < 8; j++) {
+					blanco = (i + j) % 2 == 0;
+					// Si es blanco, imprimir espacios
+					// Si es negro, imprimir '#'
+					for (int tamaño = 0; tamaño < num; tamaño++) {
+						if (blanco) {
+							System.out.print(" ");
+						} else {
+							System.out.print("#");
+						}
+					}
 				}
 				System.out.println();
 			}
-			System.out.println();
 		}
 	}
 
